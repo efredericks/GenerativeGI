5f6060cae2d08ad1c1b94bc5386d97fc7055acda
Jared Moore <moorejar@gvsu.edu>
2022-12-07 13:14:12 -0500

    Initial establishment of a DEAP implementation.  Fitness might not be working as it should and need to figure out how to save the individuals to files for tracking purposes.

--- generative_object.py
+++ generative_object.py
@@ -5,18 +5,29 @@ import math
 from techniques import *
 
 class GenerativeObject:
-  def __init__(self, idx, dim, grammar, background="black"):
+  _id = 0 # Global genome identifier
+
+  @classmethod
+  def __get_new_id(cls):
+      cls._id += 1
+      return cls._id
+
+  def __init__(self, dim, grammar, background="black", idx=-1):
     self.id = idx
     self.dim = dim
     self.grammar = grammar
     self.image = Image.new("RGBA", dim, background)
     self.isEvaluated = False
-    self.fitness = 0.0
+    self.fitness_internal = 0.0
 
   def setFitness(self, val):
-      self.fitness = val
+      self.fitness_internal = val
   def getFitness(self):
-      return self.fitness
+      return self.fitness_internal
+
+  def get_new_id(self):
+    """ Get a new id for the genome. """
+    self._id = GenerativeObject.__get_new_id()
 
   #def evaluate(self):
   #  self.isEvaluated = True
