2ca60b3d83a614c392f8a5c9d8deccb3a023c4c8
acdiller <dillerab@mail.gvsu.edu>
2022-12-11 02:48:09 -0500

    implemented circle packing, another flow field, and colour palettes

--- technique_tester.py
+++ technique_tester.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw
 from techniques import *
 from time import sleep
+from colour_palettes import palettes
 
 DIM = (1000,1000)
 background = "black"
@@ -8,8 +9,8 @@ background = "black"
 if __name__ == "__main__":
     image = Image.new("RGBA", DIM, background)
 
-    drunkenWalk(image)
-    image.save("drunk.png")
+    #drunkenWalk(image)
+    #image.save("drunk.png")
 
     #WolframCA(image)
     #image.save("wolfram.png")
@@ -18,3 +19,8 @@ if __name__ == "__main__":
     #image.save("temp.png")
     #image = simpleDither(image)
     #image.save("temp.dither.png")
+    
+    #flowField2(image, random.choice(palettes), 'curvy', random.randrange(200, 600), random.randrange(2, 5))
+    #image.save("ff.png")
+    circlePacking(image, random.choice(palettes), random.randrange(10, 30))
+    image.save("circles.png")
